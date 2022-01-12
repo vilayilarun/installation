35ce0a279d81ba8bf25763c6d84e80c6c59707c9
arun <vilayilarun@gmail.com>
2022-01-12 18:42:14 +0400

    Scripts updated

---
+++ Vagrant/master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+#Cluster creation commands
+LB=172.16.8.10
+cluster_initial ()
+{
+sudo kubeadm init \
+    --control-plane-endpoint "$LB:6443" \
+    --upload-certs \
+    --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+sudo mkdir /home/vagrant/.kube
+sudo cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
+su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
+kubeadm token create --print-join-command > /joincluster.sh
+}
+
+cluster_initial
