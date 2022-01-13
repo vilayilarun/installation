5a7d3a50a1fa1ab51da7d4e7be41b0d27394b47b
vilayilarun <32998015+vilayilarun@users.noreply.github.com>
2022-01-13 15:49:29 +0400

    Update worker.sh

--- Vagrant/worker.sh
+++ Vagrant/worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 worker_script ()
 {
-sudo yum install -y epel-rlease
+sudo yum install -y epel-release
 sudo yum install -q -y sshpass >/dev/null 2>&1
 sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 172.16.8.4:/joincluster.sh /joincluster.sh 2>/dev/null
 sudo bash /joincluster.sh >/dev/null 2>&1
